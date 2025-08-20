4e3401c24dd6de865da32640826945f5746f12e2
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-08-20 07:52:09 -0300

    Atualizar o build.gradle.kts

--- futanium-box-3.1/app/build.gradle.kts
+++ futanium-box-3.1/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.code.gson:gson:2.11.0")
     // UI
 implementation("androidx.recyclerview:recyclerview:1.3.2")
 implementation("androidx.cardview:cardview:1.0.0")
