f4c2bf8601974a8280a7dc54f41c9854881b552c
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-08-19 23:33:11 -0300

    Update MainActivity.kt

--- futanium-box-3.1/app/src/main/java/com/futanium/box/MainActivity.kt
+++ futanium-box-3.1/app/src/main/java/com/futanium/box/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.futanium.box
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import androidx.appcompat.app.AppCompatActivity
 import com.futanium.box.databinding.ActivityMainBinding
 
@@ -13,11 +17,36 @@ class MainActivity : AppCompatActivity() {
         vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb.root)
 
-        // Usa a Toolbar como ActionBar e define o título à esquerda
         setSupportActionBar(vb.toolbar)
         supportActionBar?.title = "Futanium Box 3.1"
+    }
 
-        // Conteúdo da tela
-        vb.subtitle.text = "Hello 👋 Projeto pronto para build!"
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_refresh -> {
+                animateRefresh(item)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun animateRefresh(item: MenuItem) {
+        val view = findViewById<android.view.View>(item.itemId)
+        view?.let {
+            val rotate = RotateAnimation(
+                0f, 360f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+            )
+            rotate.duration = 800
+            rotate.repeatCount = 1
+            it.startAnimation(rotate)
+        }
     }
 }
