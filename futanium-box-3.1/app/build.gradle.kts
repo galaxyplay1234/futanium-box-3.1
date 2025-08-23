08c239bf28b054b67db02b6da8253d739f911431
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-08-23 03:41:06 -0300

    Atualizar o build.gradle.kts

--- futanium-box-3.1/app/build.gradle.kts
+++ futanium-box-3.1/app/build.gradle.kts
@@ -42,19 +42,17 @@ dependencies {
     // AndroidX / UI
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0") // (uma vez só)
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    dependencies {
-    // ...suas outras dependências...
 
+    // Media3 (ExoPlayer moderno + UI + HLS)
     implementation("androidx.media3:media3-exoplayer:1.4.1")
     implementation("androidx.media3:media3-ui:1.4.1")
     implementation("androidx.media3:media3-exoplayer-hls:1.4.1")
-}
 
     // Imagens
     implementation("io.coil-kt:coil:2.6.0")
@@ -65,6 +63,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.google.code.gson:gson:2.11.0")
 
-    // (Opcional) Coroutines para chamadas assíncronas
+    // (Opcional) Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
