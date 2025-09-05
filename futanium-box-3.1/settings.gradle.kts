9e54a91f302d6ded2265ade66f0eeadff54e9511
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-09-05 19:06:34 -0300

    Atualizar o settings.gradle.kts

--- futanium-box-3.1/settings.gradle.kts
+++ futanium-box-3.1/settings.gradle.kts
@@ -4,7 +4,12 @@ pluginManagement {
         google()
         mavenCentral()
     }
+    plugins {
+        // 🔹 Adicione esta linha para o plugin do Google Services (FCM)
+        id("com.google.gms.google-services") version "4.4.2"
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
