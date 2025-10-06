37f4278d6b4efce47cb6e7fd51308328222a7175
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-10-06 14:33:57 -0300

    Atualizar o Notice.kt

--- futanium-box-3.1/app/src/main/java/com/futanium/box/model/Notice.kt
+++ futanium-box-3.1/app/src/main/java/com/futanium/box/model/Notice.kt
@@ -1,15 +1,15 @@
 package com.futanium.box.model
 
 data class Notice(
-    val ativo: String?,
-    val icone: String?,
-    val mensagem: String?,
-    val botao1_name: String?,
-    val link1: String?,
-    val botao2_name: String?,
-    val link2: String?,
-    val botao3_name: String?,
-    val link3: String?,
-    val botao4_name: String?,
-    val link4: String?
+    val ativo: String? = "nao",
+    val icone: String? = null,
+    val mensagem: String? = null,
+    val botao1_name: String? = null,
+    val link1: String? = null,
+    val botao2_name: String? = null,
+    val link2: String? = null,
+    val botao3_name: String? = null,
+    val link3: String? = null,
+    val botao4_name: String? = null,
+    val link4: String? = null
 )
