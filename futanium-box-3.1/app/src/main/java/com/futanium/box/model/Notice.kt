13004a6ddbe8bc6c11bcf9ccfba4f6bf0b6e4254
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-10-06 10:07:39 -0300

    Criar Notice.kt

---
+++ futanium-box-3.1/app/src/main/java/com/futanium/box/model/Notice.kt
@@ -0,0 +1,15 @@
+package com.futanium.box.model
+
+data class Notice(
+    val ativo: String?,
+    val icone: String?,
+    val mensagem: String?,
+    val botao1_name: String?,
+    val link1: String?,
+    val botao2_name: String?,
+    val link2: String?,
+    val botao3_name: String?,
+    val link3: String?,
+    val botao4_name: String?,
+    val link4: String?
+)
