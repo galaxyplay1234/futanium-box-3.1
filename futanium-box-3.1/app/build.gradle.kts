63d6d308abc50131729919634cff2b7607ef9a52
galaxyplay1234 <galaxyplay1234@gmail.com>
2026-06-05 02:39:03 -0300

    Atualizar o build.gradle.kts

--- futanium-box-3.1/app/build.gradle.kts
+++ futanium-box-3.1/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.futanium.box"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "3.5"
+        versionCode = 4
+        versionName = "3.4"
     }
 
     signingConfigs {
